e56a5d4cb1cfbd70e4818080329f7626b73488f6
hp <hp>
2017-11-17 09:24:23 +0800

    d commit

---
+++ 异常处理/yichangj.py
@@ -0,0 +1,33 @@
+Python 2.7.14 (v2.7.14:84471935ed, Sep 16 2017, 20:25:58) [MSC v.1500 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+================== RESTART: C:/Users/hp/Desktop/yichang.py ==================
+内容写入文件成功
+>>> 
+================== RESTART: C:/Users/hp/Desktop/yichang.py ==================
+Error: 没有找到文件或读取文件失败
+>>> 
+================== RESTART: C:/Users/hp/Desktop/yichang.py ==================
+鍏抽棴鏂囦欢
+>>> 
+================== RESTART: C:/Users/hp/Desktop/yichang.py ==================
+close this file
+
+>>> 
+================== RESTART: C:/Users/hp/Desktop/yichang.py ==================
+>>> 
+================== RESTART: C:/Users/hp/Desktop/yichang.py ==================
+>>> 
+>>> 
+================== RESTART: C:/Users/hp/Desktop/yichang.py ==================
+>>> 
+================== RESTART: C:/Users/hp/Desktop/yichang.py ==================
+finally always run.....
+
+Traceback (most recent call last):
+  File "C:/Users/hp/Desktop/yichang.py", line 33, in <module>
+    functionName(0)
+  File "C:/Users/hp/Desktop/yichang.py", line 30, in functionName
+    raise Exception("Invalid level!", level)
+Exception: ('Invalid level!', 0)
+>>> 
